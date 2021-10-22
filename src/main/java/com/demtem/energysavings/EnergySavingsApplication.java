4f52dcf3e1e0a47c14ac220dcb4a55bfaaa5c736
temilade05 <temioladugba@gmail.com>
2021-10-22 22:57:52 +0100

    initial commit

---
+++ src/main/java/com/demtem/energysavings/EnergySavingsApplication.java
@@ -0,0 +1,13 @@
+package com.demtem.energysavings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergySavingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergySavingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demtem/energysavings/EnergySavingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demtem.energysavings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergySavingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
